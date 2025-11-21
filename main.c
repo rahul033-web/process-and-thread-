3567396908955164bd2ea14308e2cabe07f0846c
rahul033-web <rs6621008344@gmail.com>
2025-11-21 10:40:40 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,40 +1,91 @@
 #include <stdio.h>
+
+#include <stdlib.h>
+
 #include "../include/scheduler.h"
 
+#include "../include/thread_control.h"
 
 int main() {
-    int n, choice, quantum;
 
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
-    Process p[n];
+    int n;
 
-    for (int i = 0; i < n; i++) {
-        p[i].pid = i + 1;
-        printf("Enter Arrival Time, Burst Time, Priority for P%d: ", i + 1);
-        scanf("%d %d %d", &p[i].arrival_time, &p[i].burst_time, &p[i].priority);
-    }
+    int choice;
 
-    printf("Select Scheduling Algorithm:\n");
-printf("1. FCFS\n2. SJF\n3. Round Robin\n4. Priority\n");
-printf("Enter choice: ");
+    int quantum;
 
+    scanf(
+        "%d",
+        &n
+    );
+    Process p[
+        n
+    ];
+    for (
+        int i = 0;
+        i < n;
+        i++
+    ) {
+        p[i].pid =
+            i + 1;
+        scanf(
+            "%d %d %d",
 
-    scanf("%d", &choice);
+            &p[i].arrival_time,
 
-    switch (choice) {
-        case 1: fcfs(p, n); break;
-        case 2: sjf(p, n); break;
-        case 3: priorityScheduling(p, n); break;
-        case 4:
-            printf("Enter time quantum: ");
-            scanf("%d", &quantum);
-            roundRobin(p, n, quantum);
-            break;
+            &p[i].burst_time,
+
+            &p[i].priority
             
-
-        default: printf("Invalid choice!\n");
+        );
     }
-
-    return 0;
+    scanf(
+        "%d",
+        &choice
+    );
+    switch (
+        choice
+    ) {
+        case 1:
+            fcfs(
+                p,
+                n
+            );
+            break;
+        case 2:
+            sjf(
+                p,
+                n
+            );
+            break;
+        case 3:
+            priorityScheduling(
+                p,
+                n
+            );
+            break;
+        case 4:
+            scanf(
+                "%d",
+                &quantum
+            );
+            roundRobin(
+                p,
+                n,
+                quantum
+            );
+            break;
+        default:
+            printf(
+                "{\"error\":\"Invalid Choice\"}"
+            );
+            return
+                0;
+    }
+    print_json(
+        p,
+        n
+    );
+    return
+        0;
 }
